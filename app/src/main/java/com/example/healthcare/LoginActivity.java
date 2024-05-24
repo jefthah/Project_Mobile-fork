9006d3166152a3c59dc8175b8aa427a3ccf29a64
Kana <heydarpejaten@gmail.com>
2024-05-24 22:38:52 +0700

    menambahkan menu register

--- app/src/main/java/com/example/healthcare/LoginActivity.java
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.healthcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,13 @@ public class LoginActivity extends AppCompatActivity {
                 }else {
                     Toast.makeText(getApplicationContext(),"Login Berhasil",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
     }
